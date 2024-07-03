ec76dec2294d76f4c0a8638ddf77e5edc547cb68
j.bektemirov <b.o.jakhongir@gmail.com>
2024-07-03 12:02:51 +0500

    add card in tracking page

--- src/features/tracking/Tracking.tsx
+++ src/features/tracking/Tracking.tsx
@@ -1,20 +1,16 @@
+import ContentCard from "../../ui/ContentCard";
+
 function Tracking() {
+
   return (
     <div className="content">
       <div className="content__header">
-        {/* SELECT FILTERLAR UCHUN */}
-        {/* ./ui/ContentHeader.tsx */}
       </div>
       <div className="content__report">
-        {/* HISOBOT (REPORTING) */}
-        {/* ./ui/ContentReport.tsx */}
-        {/* Bu feature ba'zi pagelarda bo'maydi!*/}
       </div>
       <div className="content__main">
         <div className="content__cards">
-          <div className="content__card card">
-            <div className="card__header"></div>
-          </div>
+          <ContentCard pagename='tracking' />
         </div>
       </div>
     </div>
---
+++ src/ui/ContentCard.tsx
@@ -0,0 +1,156 @@
+import { Dropdown, DropdownProps, MenuProps, Space } from "antd";
+import { useState } from "react";
+
+type PageNameType = 'tracking' | 'lead';
+
+type ContentCardProps = {
+  pagename: PageNameType;
+};
+
+function ContentCard({ pagename }: ContentCardProps) {
+  const [isOpenMenu, setOpenMenu] = useState(false);
+
+  console.log('pagename', pagename);
+  const itemsMenu: MenuProps['items'] = [
+    {
+      key: '1',
+      label: (
+        <p className="d-flex align-center">
+          <img src="/img/card/menu/destination.svg" alt="" />
+          <span className="card__menu--text ml-10">Место нахождения</span>
+        </p>
+      ),
+      className: 'mb-4'
+    },
+    {
+      key: '2',
+      label: (
+        <p className="d-flex align-center">
+          <img src="/img/card/menu/comment.svg" alt="" />
+          <span className="card__menu--text ml-10">Оставить коментарий</span>
+        </p>
+      ),
+      className: 'mb-4'
+    },
+    {
+      key: '3',
+      label: (
+        <p className="d-flex align-center">
+          <img src="/img/card/menu/edit.svg" alt="" />
+          <span className="card__menu--text ml-10">Изменить профиль</span>
+        </p>
+      ),
+      className: 'mb-4'
+    },
+    {
+      key: '4',
+      label: (
+        <p className="d-flex align-center card__menu--label card__menu--label-delete">
+          <img src="/img/card/menu/delete.svg" alt="" />
+          <span className="card__menu--text ml-10" style={{ color: '#FF2D55' }}>Удалить из списка</span>
+        </p>
+      ),
+      className: 'card__menu--label-delete'
+    },
+  ];
+
+  const handleOpenMenu: DropdownProps['onOpenChange'] = (nextOpen, info) => {
+    if (info.source === 'trigger' || nextOpen) {
+      setOpenMenu(nextOpen);
+    }
+  };
+
+  return (
+    <div className="content__card card">
+      <div className="card__header">
+        <div className="card__user">
+          <p className="card__user--avatar">
+            <img src="/img/card/empty-avatar.svg" alt="avatar" />
+          </p>
+          <div className="card__user--info">
+            <p className="name">Jahongir Umirzaqov</p>
+            <p className="rate"><span>4.5</span><img src="/img/card/star.svg" alt="rate" /></p>
+          </div>
+        </div>
+        <Dropdown
+          menu={{
+            items: itemsMenu,
+            selectable: false,
+            defaultSelectedKeys: [''],
+          }}
+          placement="bottom"
+          trigger={['click']}
+          arrow={{ pointAtCenter: true }}
+          open={isOpenMenu}
+          destroyPopupOnHide={true}
+          onOpenChange={handleOpenMenu}
+          className="drawer-header__settings"
+        >
+          <div className="card__dots cursor-pointer">
+            <Space>
+              <img src="/img/card/dots.svg" alt="dots" />
+            </Space>
+          </div>
+        </Dropdown>
+      </div>
+      <div className="card__items">
+        <div className="card__item">
+          <div className="card__item--label">
+            <img src="/img/card/phone.svg" alt="" />
+            <span>Телефон</span>
+          </div>
+          <div className="card__item--value">+998 91 335 09 15</div>
+        </div>
+        <div className="card__item">
+          <div className="card__item--label">
+            <img src="/img/card/car.svg" alt="" />
+            <span>Тип машины</span>
+          </div>
+          <div className="card__item--value">Nexia 2</div>
+        </div>
+        <div className="card__item">
+          <div className="card__item--label">
+            <img src="/img/card/date.svg" alt="" />
+            <span>Дата регистрации</span>
+            <span className="d-none">Активность
+              водителя</span>
+          </div>
+          <div className="card__item--value">2024.05 15:00</div>
+        </div>
+        <div className="card__item">
+          <div className="card__item--label">
+            <img src="/img/card/book.svg" alt="" />
+            <span>Компания</span>
+          </div>
+          <div className="card__item--value">Coca Cola</div>
+        </div>
+        <div className="card__item card__item--comment">
+          <div className="card__item--label">
+            <img src="/img/card/comment.svg" alt="" />
+            <span>Комментарий</span>
+          </div>
+          <div className="card__item--value">Проехать с улицы Алишер навои
+            до улицы фараби 15:00 - 16:00 20.05.2024</div>
+        </div>
+        <div className="card__item">
+          <div className="card__item--label">
+            <img src="/img/card/loading.svg" alt="" />
+            <span>Статус фотоотчета</span>
+          </div>
+          <div className="card__item--value card__item--value-status">
+            <span className="dot-live"></span>
+            фото отчет
+            отправлен</div>
+        </div>
+      </div>
+      <div className="card__bottom">
+        <button className="card__bottom--btn">
+          <img src="/img/card/location.svg" alt="" />
+          <span className="ml-5">Показать последнюю активность</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ContentCard;
