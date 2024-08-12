896ebdf13a0c79ec65f4174392ef22833a2024f7
j.bektemirov <b.o.jakhongir@gmail.com>
2024-08-12 18:35:46 +0500

    change operator page

--- src/features/operator/Operator.tsx
+++ src/features/operator/Operator.tsx
@@ -1,5 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react';
+import ContentHeader from '../../ui/ContentHeader';
+import EmptyCard from '../../ui/EmptyCard';
+import TrackContentCard from '../tracks/TrackContentCard';
+import { useTracks } from '../tracks/useTracks';
 function Operator() {
-  return <div>Operator</div>;
+  const [isOpenEditModal, setOpenEditModal] = useState(false);
+  const [currentDataId, setCurrentDataId] = useState('');
+  const { data, isLoading } = useTracks();
+
+  if (isLoading && !Object.keys(data || {})?.length) {
+    return;
+  }
+
+  return (
+    <div className="content">
+      <div className="content__header">
+        <ContentHeader pagename="track" />
+      </div>
+      <div className="content__report"></div>
+      <div className="content__main">
+        <div className="content__cards">
+          <div className="content__row">
+            {data?.results?.length > 0 ? (
+              (data?.results || []).map((item: { id: string }) => (
+                <TrackContentCard
+                  key={item.id}
+                  item={item}
+                  pagename="track"
+                  onEdit={() => {
+                    setCurrentDataId('');
+                    setOpenEditModal(true);
+                    setTimeout(() => setCurrentDataId(item.id), 0);
+                  }}
+                />
+              ))
+            ) : (
+              <EmptyCard text="operator" />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Operator;
---
+++ src/services/operator/index.ts
@@ -0,0 +1,135 @@
+import { AxiosError } from 'axios';
+import apiClient from '../axios';
+
+type ApiErrorResponse = {
+  message: string;
+};
+
+class Tracks {
+  private $api: typeof apiClient;
+
+  constructor() {
+    this.$api = apiClient;
+  }
+
+  // GET: /tracking/list/ //todo
+  async getOperators() {
+    try {
+      const { data } = await this.$api.get(
+        '/tracking/list/',
+        //    {
+        //   params: { name, limit, offset, status, q },
+        // }
+      );
+      return data;
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiErrorResponse>;
+      throw new Error(
+        axiosError.response?.data?.message || 'An unknown error occurred',
+      );
+    }
+  }
+
+  // GET: /tracking/retrieve/:id
+  async getTrack(id: string) {
+    try {
+      const { data } = await this.$api.get(`/tracking/retrieve/${id}`);
+      return data;
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiErrorResponse>;
+      throw new Error(
+        axiosError.response?.data?.message || 'An unknown error occurred',
+      );
+    }
+  }
+
+  // DELETE: /tracking/delete/:id/
+  async deleteTrack(id: string) {
+    try {
+      const { data } = await this.$api.delete(`/tracking/delete/${id}/`);
+      return data;
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiErrorResponse>;
+      throw new Error(
+        axiosError.response?.data?.message || 'An unknown error occurred',
+      );
+    }
+  }
+
+  // UPDATE: /tracking/update/:id/
+  async updateTrack({ ...payload }) {
+    try {
+      const response = await this.$api.put(`/tracking/update/${payload.id}/`, {
+        ...payload,
+      });
+      if (response && response.data) {
+        return response.data;
+      } else {
+        throw new Error('The API response did not contain any data.');
+      }
+    } catch (error) {
+      console.log('ERR', error);
+      const axiosError = error as AxiosError<ApiErrorResponse>;
+      throw new Error(
+        axiosError.response?.data?.message || 'An unknown error occurred',
+      );
+    }
+  }
+
+  // POST: /comment/create/
+  async createComment({ ...payload }) {
+    try {
+      const response = await this.$api.post(`/comment/create/`, {
+        ...payload,
+      });
+      if (response && response.data) {
+        return response.data;
+      } else {
+        throw new Error('The API response did not contain any data.');
+      }
+    } catch (error) {
+      console.log('ERR', error);
+      const axiosError = error as AxiosError<ApiErrorResponse>;
+      throw new Error(
+        axiosError.response?.data?.message || 'An unknown error occurred',
+      );
+    }
+  }
+
+  // POST: /carriers/create/
+  async createCarrier({
+    name,
+    address,
+    mcNumber,
+    contactName,
+    phone,
+    phone2,
+    email,
+    fax,
+    status,
+    location,
+  }: CreateCarrierParams) {
+    try {
+      const { data } = await this.$api.post('/carriers/create/', {
+        name,
+        address,
+        mcNumber,
+        contactName,
+        phone,
+        phone2,
+        email,
+        fax,
+        status,
+        location,
+      });
+      return data;
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiErrorResponse>;
+      throw new Error(
+        axiosError.response?.data?.message || 'An unknown error occurred',
+      );
+    }
+  }
+}
+
+export default new Tracks();
